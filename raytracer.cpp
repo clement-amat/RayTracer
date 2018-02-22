a6a5a8922116742df4b068a066f96acb8eb197bd
Unknown <clemamat@gmail.com>
2018-02-22 19:53:32 +0100

    First commit

---
+++ raytracer.cpp
@@ -0,0 +1,261 @@
+
+#include "raytracer.h"
+#include "scene_types.h"
+#include "ray.h"
+#include "image.h"
+#include "kdtree.h"
+#include <stdio.h>
+
+
+/// acne_eps is a small constant used to prevent acne when computing intersection
+//  or boucing (add this amount to the position before casting a new ray !
+const float acne_eps = 1e-4;
+
+bool intersectPlane(Ray *ray, Intersection *intersection, Object *plane) {
+  vec3 n = plane->geom.plane.normal;
+  vec3 d = ray->dir;
+  float dist = plane->geom.plane.dist;
+  float dotDN = dot(d, n);
+  
+  if (dotDN != 0.f) {
+    point3 o = ray->orig;
+    float t;
+
+    t = -(dot(o,n) + dist) / dotDN;
+    if (t >= ray->tmin && t <= ray->tmax) {
+      ray->tmax = t;
+      // normal intersection = normal au plan
+      // pt intersect : o + t* direction
+      intersection->normal = n;
+      intersection->position = o + t * d;
+      return true;
+    }
+
+  }
+  return false;
+
+}
+
+bool intersectSphere(Ray *ray, Intersection *intersection, Object *obj) {
+  float a = 1;
+  float b = 2 * dot(ray->dir, (ray->orig - obj->geom.sphere.center));
+  vec3 oMinusC = (ray->orig - obj->geom.sphere.center);
+  float c = dot(oMinusC,oMinusC) 
+              - obj->geom.sphere.radius * obj->geom.sphere.radius;
+
+  float delta = b * b - 4 * a * c;
+
+  if (delta == 0) {     // une solution
+    float t = (- b - sqrt(delta)) / 2.f * a;
+    if (t >= ray->tmin && t <= ray->tmax) {
+      intersection->position = ray->orig + t * ray->dir;
+      intersection->normal = normalize(intersection->position - c);
+      return true;
+    }
+
+    return false;  
+  } else if (delta > 0) {    // deux solutions 
+      float t1 = (- b - sqrt(delta)) / 2.f * a;
+      float t2 = (- b + sqrt(delta)) / 2.f * a;
+
+      if (t1 >= ray->tmin && t1 <= ray->tmax && t2 >= ray->tmin && t2 <= ray->tmax) {
+
+        if (t1 <= t2) {
+          intersection->position = ray->orig + t1 * ray->dir;
+        } else {
+          intersection->position = ray->orig + t2 * ray->dir;
+        }
+        intersection->normal = normalize(intersection->position - c);
+        return true;
+
+      } else if (t1 >= ray->tmin && t1 <= ray->tmax) {    // seul t1 valide
+        intersection->position = ray->orig + t1 * ray->dir;
+        intersection->normal = normalize(intersection->position - c);
+        return true;
+
+      } else if (t2 >= ray->tmin && t2 <= ray->tmax) {    // seul t2 valide
+        intersection->position = ray->orig + t2 * ray->dir;
+        intersection->normal = normalize(intersection->position - c);
+        return true;
+      }
+  }
+  
+  // pas de pts d'intersection valide
+  return false;
+
+}
+
+bool intersectScene(const Scene *scene, Ray *ray, Intersection *intersection) {
+  size_t objectCount = scene->objects.size();
+  bool hasIntersection = false;
+
+  //!\todo loop on each object of the scene to compute intersection
+  for (size_t i = 0 ; i < objectCount ; i++) {
+    if (scene->objects[i]->geom.type == SPHERE) {
+      if (intersectSphere(ray, intersection, scene->objects[i])) {
+        hasIntersection = true;
+      }
+    } else if (scene->objects[i]->geom.type == PLANE){
+      if (intersectPlane(ray, intersection, scene->objects[i])) {
+        hasIntersection = true;
+      }
+    }
+  }
+
+  return hasIntersection;
+}
+
+/* -------------------- ------------------------------------------------------- */
+/*
+ *	The following functions are coded from Cook-Torrance bsdf model description and are suitable only
+ *  for rough dielectrics material (RDM. Code has been validated with Mitsuba renderer)
+ */
+
+/** Normal Distribution Function : Beckmann
+ * NdotH : Norm . Half
+ */
+float RDM_Beckmann(float NdotH, float alpha) {
+
+
+  //! \todo compute Beckmann normal distribution
+  return 0.5f;
+
+}
+
+// Fresnel term computation. Implantation of the exact computation. we can use the Schlick approximation
+// LdotH : Light . Half
+float RDM_Fresnel(float LdotH, float extIOR, float intIOR) {
+
+  //! \todo compute Fresnel term
+  return 0.5f;
+
+}
+
+
+// Shadowing and masking function. Linked with the NDF. Here, Smith function, suitable for Beckmann NDF
+float RDM_chiplus(float c) {
+  return (c > 0.f) ? 1.f : 0.f;
+}
+
+// DdotH : Dir . Half
+// HdotN : Half . Norm
+float RDM_G1(float DdotH, float DdotN, float alpha) {
+
+  //!\todo compute G1 term of the Smith fonction
+  return 0.5f;
+
+}
+
+// LdotH : Light . Half
+// LdotN : Light . Norm
+// VdotH : View . Half
+// VdotN : View . Norm
+float RDM_Smith(float LdotH, float LdotN, float VdotH, float VdotN, float alpha) {
+
+  //!\todo the Smith fonction
+  return 0.5f;
+
+
+}
+
+// Specular term of the Cook-torrance bsdf
+// LdotH : Light . Half
+// NdotH : Norm . Half
+// VdotH : View . Half
+// LdotN : Light . Norm
+// VdotN : View . Norm
+color3 RDM_bsdf_s(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m) {
+
+  //!\todo specular term of the bsdf, using D = RDB_Beckmann, F = RDM_Fresnel, G = RDM_Smith
+  return color3(.5f);
+
+  
+}
+// diffuse term of the cook torrance bsdf
+color3 RDM_bsdf_d(Material *m) {
+
+  //!\todo compute diffuse component of the bsdf
+  return color3(.5f);
+
+}
+
+// The full evaluation of bsdf(wi, wo) * cos (thetai)
+// LdotH : Light . Half
+// NdotH : Norm . Half
+// VdotH : View . Half
+// LdotN : Light . Norm
+// VdtoN : View . Norm
+// compute bsdf * cos(Oi)
+color3 RDM_bsdf(float LdotH, float NdotH, float VdotH, float LdotN, float VdotN, Material *m) {
+
+  //! \todo compute bsdf diffuse and specular term
+  return color3(0.f);
+
+}
+
+
+
+
+/* --------------------------------------------------------------------------- */
+
+color3 shade(vec3 n, vec3 v, vec3 l, color3 lc, Material *mat ){
+  color3 ret = color3(0.f);
+
+  //! \todo compute bsdf, return the shaded color taking into account the
+  //! lightcolor
+  
+
+  return ret;
+	    
+}
+
+//! if tree is not null, use intersectKdTree to compute the intersection instead of intersect scene
+color3 trace_ray(Scene * scene, Ray *ray, KdTree *tree) {  
+  Intersection intersection;
+
+  if (intersectScene(scene, ray, &intersection)) {
+    return color3(0.5f * intersection.normal + 0.5f);
+  } else {
+    return scene->skyColor;
+  }
+}
+
+void renderImage(Image *img, Scene *scene) {
+
+  //! This function is already operational, you might modify it for antialiasing and kdtree initializaion
+  float aspect = 1.f/scene->cam.aspect;
+    
+  KdTree *tree =  NULL;
+
+
+  //! \todo initialize KdTree
+
+  float delta_y = 1.f / (img->height * 0.5f); //! one pixel size
+  vec3 dy = delta_y * aspect * scene->cam.ydir; //! one pixel step 
+  vec3 ray_delta_y = (0.5f - img->height * 0.5f) / (img->height * 0.5f) * aspect * scene->cam.ydir;
+
+  float delta_x = 1.f / (img->width * 0.5f);
+  vec3 dx = delta_x * scene->cam.xdir;
+  vec3 ray_delta_x = (0.5f - img->width * 0.5f) / (img->width * 0.5f) *scene->cam.xdir;
+  
+    
+  for(size_t j=0; j<img->height; j++) {
+    if(j!=0) printf("\033[A\r");
+    float progress = (float)j/img->height*100.f;
+    printf("progress\t[");
+    int cpt = 0;
+    for(cpt = 0; cpt<progress; cpt+=5) printf(".");
+    for(       ; cpt<100; cpt+=5) printf(" ");
+    printf("]\n");
+#pragma omp parallel for
+    for(size_t i=0; i<img->width; i++) {
+      color3 *ptr = getPixelPtr(img, i,j);
+      vec3 ray_dir = scene->cam.center + ray_delta_x + ray_delta_y + float(i)*dx + float(j)*dy;
+
+      Ray rx;
+      rayInit(&rx, scene->cam.position, normalize(ray_dir));
+      *ptr = trace_ray(scene, &rx, tree);
+
+    }
+  }
+}
---
+++ raytracer.d
@@ -0,0 +1,240 @@
+raytracer.o raytracer.d : raytracer.cpp /usr/include/stdc-predef.h raytracer.h \
+ defines.h /usr/lib/gcc/x86_64-linux-gnu/7/include/stdbool.h \
+ glm-0.9.8.4/glm/glm/glm.hpp glm-0.9.8.4/glm/glm/detail/_fixes.hpp \
+ /usr/include/c++/7/cmath \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/cpu_defines.h \
+ /usr/include/c++/7/bits/cpp_type_traits.h \
+ /usr/include/c++/7/ext/type_traits.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val_flt128.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h \
+ /usr/include/c++/7/bits/std_abs.h /usr/include/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/c++/7/climits \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/uio_lim.h /usr/include/c++/7/cfloat \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/float.h \
+ /usr/include/c++/7/limits /usr/include/c++/7/cassert \
+ /usr/include/assert.h glm-0.9.8.4/glm/glm/fwd.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_int.hpp \
+ glm-0.9.8.4/glm/glm/detail/setup.hpp /usr/include/c++/7/cstddef \
+ glm-0.9.8.4/glm/glm/detail/../simd/platform.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/mm_malloc.h \
+ /usr/include/c++/7/stdlib.h /usr/include/c++/7/cstdlib \
+ /usr/include/c++/7/type_traits /usr/include/c++/7/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ glm-0.9.8.4/glm/glm/detail/type_float.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec.hpp \
+ glm-0.9.8.4/glm/glm/detail/precision.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_mat.hpp glm-0.9.8.4/glm/glm/vec2.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec2.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec2.inl glm-0.9.8.4/glm/glm/vec3.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec3.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec3.inl glm-0.9.8.4/glm/glm/vec4.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec4.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_vec4.inl \
+ glm-0.9.8.4/glm/glm/detail/type_vec4_simd.inl \
+ glm-0.9.8.4/glm/glm/mat2x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_mat2x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_mat2x2.inl \
+ glm-0.9.8.4/glm/glm/detail/func_matrix.hpp \
+ glm-0.9.8.4/glm/glm/detail/../mat2x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat2x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat2x3.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat2x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat2x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat2x4.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat3x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x2.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat3x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x3.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat3x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat3x4.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat4x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x2.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x2.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat4x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x3.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x3.inl \
+ glm-0.9.8.4/glm/glm/detail/../mat4x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x4.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x4.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_mat4x4_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/func_matrix.inl \
+ glm-0.9.8.4/glm/glm/detail/../geometric.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_geometric.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_geometric.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_exponential.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_vec1.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/type_vec1.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_exponential.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_vector_relational.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_vector_relational.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_vector_relational_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/_vectorize.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_exponential_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/../simd/exponential.h \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_common.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/_fixes.hpp \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_common.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_common_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/../simd/common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512fintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512erintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512pfintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512cdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512bwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512dqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vlbwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vldqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512ifmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512ifmavlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vbmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vbmivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx5124fmapsintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx5124vnniwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/avx512vpopcntdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/shaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/xtestintrin.h \
+ glm-0.9.8.4/glm/glm/detail/../detail/func_geometric_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../detail/../simd/geometric.h \
+ glm-0.9.8.4/glm/glm/detail/func_matrix_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../simd/matrix.h \
+ glm-0.9.8.4/glm/glm/trigonometric.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_trigonometric.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_trigonometric.inl \
+ glm-0.9.8.4/glm/glm/detail/func_trigonometric_simd.inl \
+ glm-0.9.8.4/glm/glm/exponential.hpp glm-0.9.8.4/glm/glm/common.hpp \
+ glm-0.9.8.4/glm/glm/packing.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_packing.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_packing.inl \
+ glm-0.9.8.4/glm/glm/detail/type_half.hpp \
+ glm-0.9.8.4/glm/glm/detail/type_half.inl \
+ glm-0.9.8.4/glm/glm/detail/func_packing_simd.inl \
+ glm-0.9.8.4/glm/glm/matrix.hpp glm-0.9.8.4/glm/glm/vector_relational.hpp \
+ glm-0.9.8.4/glm/glm/integer.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_integer.hpp \
+ glm-0.9.8.4/glm/glm/detail/func_integer.inl \
+ glm-0.9.8.4/glm/glm/detail/func_integer_simd.inl \
+ glm-0.9.8.4/glm/glm/detail/../simd/integer.h image.h scene.h ray.h \
+ scene_types.h /usr/include/c++/7/vector \
+ /usr/include/c++/7/bits/stl_algobase.h \
+ /usr/include/c++/7/bits/functexcept.h \
+ /usr/include/c++/7/bits/exception_defines.h \
+ /usr/include/c++/7/ext/numeric_traits.h \
+ /usr/include/c++/7/bits/stl_pair.h /usr/include/c++/7/bits/move.h \
+ /usr/include/c++/7/bits/concept_check.h \
+ /usr/include/c++/7/bits/stl_iterator_base_types.h \
+ /usr/include/c++/7/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/7/debug/assertions.h \
+ /usr/include/c++/7/bits/stl_iterator.h \
+ /usr/include/c++/7/bits/ptr_traits.h /usr/include/c++/7/debug/debug.h \
+ /usr/include/c++/7/bits/predefined_ops.h \
+ /usr/include/c++/7/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/7/bits/c++allocator.h \
+ /usr/include/c++/7/ext/new_allocator.h /usr/include/c++/7/new \
+ /usr/include/c++/7/exception /usr/include/c++/7/bits/exception.h \
+ /usr/include/c++/7/bits/exception_ptr.h \
+ /usr/include/c++/7/bits/cxxabi_init_exception.h \
+ /usr/include/c++/7/typeinfo /usr/include/c++/7/bits/hash_bytes.h \
+ /usr/include/c++/7/bits/nested_exception.h \
+ /usr/include/c++/7/bits/memoryfwd.h \
+ /usr/include/c++/7/bits/stl_construct.h \
+ /usr/include/c++/7/ext/alloc_traits.h \
+ /usr/include/c++/7/bits/alloc_traits.h \
+ /usr/include/c++/7/bits/stl_uninitialized.h \
+ /usr/include/c++/7/bits/stl_vector.h /usr/include/c++/7/initializer_list \
+ /usr/include/c++/7/bits/stl_bvector.h \
+ /usr/include/c++/7/bits/functional_hash.h \
+ /usr/include/c++/7/bits/range_access.h \
+ /usr/include/c++/7/bits/vector.tcc kdtree.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h /usr/include/libio.h \
+ /usr/include/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h
